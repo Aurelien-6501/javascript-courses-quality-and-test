f216c512cc5a461b16d1d539dd971c94ecfd3b95
Aurelien <aurelien.fillion.pro@gmail.com>
2024-12-16 15:35:56 +0100

    ci

--- playwright.config.js
+++ playwright.config.js
@@ -22,9 +22,9 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'],
-    ['@estruyf/github-actions-reporter']
-  ],/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */,
+    ["list"],
+    ["@estruyf/github-actions-reporter"],
+  ] /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
